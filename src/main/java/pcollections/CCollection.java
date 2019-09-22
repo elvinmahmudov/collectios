79a4f0e770fffe816324513bfb1165a87ec3c94e
Elvin Mahmudov <elvin.mahmudov@pashabank.az>
2019-09-22 19:26:19 +0400

    Add interface for CCollection

---
+++ src/main/java/pcollections/CCollection.java
@@ -0,0 +1,17 @@
+package pcollections;
+
+import java.util.Collection;
+
+
+public interface CCollection<E> extends Collection<E> {
+
+    CCollection<E> prepend(E e);
+
+    CCollection<E> prependAll(Collection<? extends E> list);
+
+    CCollection<E> delete(E e);
+
+    CCollection<E> deleteAll(Collection<? extends E> list);
+
+    CCollection<E> subList(int start, int end);
+}

6f84175e81a71a2f71e0ff60971aca5f5e979728
Elvin Mahmudov <elvin.mahmudov@pashabank.az>
2019-09-23 22:23:00 +0400

    Add cmap iterator

--- src/main/java/pcollections/cmap/CMapIterator.java
+++ src/main/java/pcollections/cmap/CMapIterator.java
@@ -1,4 +1,32 @@
 package pcollections.cmap;
 
-public class CMapIterator {
+import pcollections.CCollection;
+import pcollections.clist.CLinkedList;
+
+import java.util.Iterator;
+
+public class CMapIterator<E> implements Iterator<E> {
+    private final Iterator<CCollection<E>> i;
+    private CCollection<E> seq = CLinkedList.empty();
+
+    CMapIterator(Iterator<CCollection<E>> i) {
+        this.i = i;
+    }
+
+    public boolean hasNext() {
+        return seq.size() > 0 || i.hasNext();
+    }
+
+    public E next() {
+        if (seq.size() == 0) {
+            seq = i.next();
+        }
+        final E result = seq.stream().findFirst().get();
+        seq = seq.subCList(1, seq.size());
+        return result;
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
 }
